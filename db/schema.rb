912273174310a4fe00a2709c708a6c0d766f7b65
x5se7en9x <tnansci@gmail.com>
2020-05-11 14:18:54 -0400

    Authorization working, Added user ownership to entries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_171641) do
+ActiveRecord::Schema.define(version: 2020_05_11_181815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_05_11_171641) do
     t.string "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
   create_table "examples", force: :cascade do |t|
@@ -42,5 +44,6 @@ ActiveRecord::Schema.define(version: 2020_05_11_171641) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "entries", "users"
   add_foreign_key "examples", "users"
 end
